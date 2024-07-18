3aaeb1671d29c49a5a6e0e25c9e2dbdb95696804
V.rahul <66204932+Rahu-3@users.noreply.github.com>
2024-07-18 08:24:56 -0500

    Time: 51 ms (27.96%), Space: 41.3 MB (6.71%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>ans;
+        int n = arr.size();
+        
+//         priority queue (min heap) to store the pair of difference and array element
+        priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>>q; 
+        
+        for(int i=0 ; i<n ; i++)
+        {
+            q.push({abs(x-arr[i]) , arr[i]});
+        }
+        
+        while(k--)
+        {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        
+        sort(ans.begin() , ans.end());
+        
+        return ans;
+    }
+};
