991e48ae3f138b16a299617b383a17dd074c7c99
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-01-10 16:05:42 -0600

    Time: 100 ms (75.97%), Space: 34.7 MB (68.25%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -2,19 +2,18 @@ class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         ms=0
         cs=0
-        j=0
-        s=set()
         n=len(nums)
+        s=set()
+        i=0
         if n<k:
             return 0
-        for i in range(n):
-            while nums[i] in s or i-j+1>k:
-                s.remove(nums[j])
-                cs-=nums[j]
-                j+=1
-            s.add(nums[i])
-            cs+=nums[i]
-            if i-j+1==k:
+        for j in range(n):
+            while nums[j]  in s or (j-i+1)>k:
+                s.remove(nums[i])
+                cs-=nums[i]
+                i+=1
+            s.add(nums[j])
+            cs+=nums[j]    
+            if j-i+1==k:
                 ms=max(ms,cs)
         return ms
-        
