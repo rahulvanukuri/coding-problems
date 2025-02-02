c2cbd1906598cf503e8d26dda416b21b49cfdbfc
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-02-01 18:03:24 -0600

    Time: 15 ms (25.29%), Space: 21.6 MB (19.17%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+     int maxh(vector<int>v){
+        int n=v.size();
+        vector<int>l(n,-1);
+        vector<int>r(n,n);
+        stack<int>p;
+        stack<int>s;
+        for(int i=0;i<n;i++){
+            while(!s.empty() && v[s.top()]>=v[i]){
+                s.pop();
+            }
+            if(!s.empty()){
+                l[i]=s.top();
+            }
+            s.push(i);
+        }
+        for(int i=n-1;i>=0;i--){
+            while(!p.empty() && v[p.top()]>=v[i]){
+                p.pop();
+            }
+            if(!p.empty()){
+                r[i]=p.top();
+            }
+            p.push(i);
+        }
+        int mx=0;
+        for(int i=0;i<n;i++){
+            int wid=r[i]-l[i]-1;
+            int ar=wid*v[i];
+            mx=max(mx,ar);
+        }
+        return mx;
+     }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+     if (matrix.empty() || matrix[0].empty()) return 0;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> his(n, 0);  
+        int maxArea = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    his[j] += 1;
+                } else {
+                    his[j] = 0;
+                }
+            }
+            maxArea = max(maxArea, maxh(his));
+        }
+
+        return maxArea;
+
+    }
+};
