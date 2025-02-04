1568d49e27960c0fdffcc5ba468a0d0e2f9a0cf2
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-02-04 03:20:12 -0600

    Time: 1567 ms (5.06%), Space: 31.3 MB (23.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,29 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        vis=set()
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         row,col=len(grid),len(grid[0])
-        p=0
+        vis=set()
+        isl=0
         def bfs(r,c):
             q=collections.deque()
             q.append((r,c))
             vis.add((r,c))
             while q:
+                dire=[[0,1],[0,-1],[1,0],[-1,0]]
                 cr,cc=q.popleft()
-                dire=[[0,1],[1,0],[0,-1],[-1,0]]
                 for dr,dc in dire:
                     nr,nc=cr+dr,cc+dc
-                    if(nr in range(len(grid)) and nc in range(col) and grid[nr][nc]=='1' and (nr,nc)not in vis):
+                    if  nr in range(row) and nc in range(col) and grid[nr][nc]=='1' and (nr,nc) not in vis:
                         q.append((nr,nc))
                         vis.add((nr,nc))
         for r in range(row):
             for c in range(col):
-                if (r,c) not in vis and grid[r][c]=='1':
+                if grid[r][c]=='1' and (r,c) not in vis:
                     bfs(r,c)
-                    p+=1
-        return p
-            
+                    isl+=1
+        return isl
+
+        
