dc7ee3f7f5f8571d598f27703c9ec5b0500384a2
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-05-12 10:21:58 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& inter) {
-        vector<vector<int>>ans;
         int n=inter.size();
-        sort(inter.begin(),inter.end());
+        vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            int st=inter[i][0];
-            int end=inter[i][1];
-            if(!ans.empty()&& end<=ans.back()[1]){
-           continue;
+            if(ans.empty()||inter[i][0]>ans.back()[1]){
+                ans.push_back(inter[i]);
             }
-            for(int j=i+1;j<n;j++){
-                if(inter[j][0]<=end){
-                    end=max(end,inter[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1]=max(ans.back()[1],inter[i][1]);
             }
-            ans.push_back({st,end});
         }
         return ans;
+        
     }
 };
