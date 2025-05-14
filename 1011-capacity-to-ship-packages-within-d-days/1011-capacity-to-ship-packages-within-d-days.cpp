fa5f70aabdb2eb56c7e5bf008423c5ab7c1b0062
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-05-13 21:58:10 -0500

    Time: 11 ms (68.41%), Space: 35.1 MB (35.44%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int findays(vector<int>&weights,int cap){
-        int d=1,l=0;
-        for(int i=0;i<weights.size();i++){
-            if(weights[i]+l>cap){
-                d+=1;
-                l=weights[i];
-            }
-            else{
-                l+=weights[i];
-            }
-        }
-        return d;
+   int find(vector<int>&v,int k){
+   int d=1,l=0;
+   for(int i=0;i<v.size();i++){
+    if(v[i]+l>k){
+        d+=1;
+        l=v[i];
     }
+    else{
+        l+=v[i];
+    }
+   }
+   return d;
+
+   }
     int shipWithinDays(vector<int>& weights, int days) {
+        int n=weights.size();
         int l=*max_element(weights.begin(),weights.end());
         int h=accumulate(weights.begin(),weights.end(),0);
         while(l<=h){
             int m=(l+h)/2;
-            int nd=findays(weights,m);
-            if(nd<=days){
+             if(find(weights,m)<=days){
                 h=m-1;
-            }
-            else{
+             }
+             else{
                 l=m+1;
-            }
+             }
         }
         return l;
     }
