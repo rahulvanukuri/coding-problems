27f838f4306fb2e3b014f4a8fdffa39807b14164
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-06-07 14:14:11 -0500

    Time: 0 ms (100%), Space: 10.5 MB (24.08%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-int cs(vector<int>&nums,int k){
-    int cnt=1,cs=0;
-    for(int i=0;i<nums.size();i++){
-        if(cs+nums[i]>k){
-            cnt+=1;
-         cs=nums[i];
+    int cs(vector<int>&nums,int k){
+        int p=0,c=1;
+        for(int i=0;i<nums.size();i++){
+            if(p+nums[i]>k){
+            c+=1;
+            p=nums[i];
+            }
+            else{
+               p+=nums[i]; 
+            }
         }
-        else{
-            cs+=nums[i];
-        }
+        return c;
     }
-    return cnt;
-}
+
     int splitArray(vector<int>& nums, int k) {
         int l=*max_element(nums.begin(),nums.end());
         int h=accumulate(nums.begin(),nums.end(),0);
         while(l<=h){
-            int m=(l+h)/2;
-            if(cs(nums,m)<=k){
-                h=m-1;
+            int m=l+(h-l)/2;
+            if(cs(nums,m)>k){
+                l=m+1;
             }
             else{
-                l=m+1;            }
+                h=m-1;
+            }
         }
         return l;
     }
