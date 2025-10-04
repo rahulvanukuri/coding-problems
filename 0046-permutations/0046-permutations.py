88d2e858ef3548fbb1aa82cc4d00ee94485fb8a8
V.rahul <66204932+rahulvanukuri@users.noreply.github.com>
2025-10-04 10:10:38 -0500

    Time: 0 ms (100%), Space: 17.9 MB (82.58%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums)==1:
+            return [nums]
         res=[]
-        n=len(nums)
-        def back(st):
-            if st==n:
-                res.append(nums[:])
-                return
-            for i in range(st,n):
-                nums[st],nums[i]=nums[i],nums[st]
-                back(st+1)
-                nums[st],nums[i]=nums[i],nums[st]
-        back(0)
+        for id,i in enumerate(nums):
+            if id==len(nums)-1:
+                new_nums=nums[:id]
+            elif id==0:
+                new_nums=nums[1:]
+            else:
+                new_nums=nums[:id]+nums[id+1:]
+            new_res=self.permute(new_nums)
+            for r in new_res:
+                res.append(r+[i])
         return res
+            
+
         
